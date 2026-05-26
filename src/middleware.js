b594e61bc47a0feb6df6e031117c25e16e0c83c0
Syazuke <ssyazukee@gmail.com>
2026-05-26 23:11:44 +0700

    update

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,7 @@
 import withAuth from "next-auth/middleware";
 
-// Mengekspor fungsi secara eksplisit agar Next.js 16 tidak error
 export default withAuth;
 
 export const config = {
-  // Masukkan halaman admin Little Blueming yang ingin dikunci
   matcher: ["/upload/:path*", "/dashboard/:path*"],
 };
